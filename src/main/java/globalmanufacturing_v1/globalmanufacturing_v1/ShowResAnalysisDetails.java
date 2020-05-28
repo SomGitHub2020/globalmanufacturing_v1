2faf99a8ffb9a571f0dd6a841053796017104ee9
SomGitHub2020 <61223781+SomGitHub2020@users.noreply.github.com>
2020-05-28 13:07:56 +0530

    r434

--- src/main/java/globalmanufacturing_v1/globalmanufacturing_v1/ShowResAnalysisDetails.java
+++ src/main/java/globalmanufacturing_v1/globalmanufacturing_v1/ShowResAnalysisDetails.java
@@ -15,7 +15,7 @@ public class ShowResAnalysisDetails {
 	
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection( "jdbc:mysql://connplantservice:3306/connplantsdb?user=root&password=VySU8WBweuVYNx3T&useSSL=false");  
+            Connection con=DriverManager.getConnection( "jdbc:mysql://globalmfgservice:3306/globalmfgdb?user=root&password=igCotOwKrnqrA310&useSSL=false");  
             Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery(
             		"SELECT DISTINCT WORKCENTER, FLOOR(RAND()*(100)) FROM `RESOURCE_MASTER` "
@@ -51,7 +51,7 @@ public class ShowResAnalysisDetails {
 		String durationlist = "";
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection( "jdbc:mysql://connplantservice:3306/connplantsdb?user=root&password=VySU8WBweuVYNx3T&useSSL=false");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://globalmfgservice:3306/globalmfgdb?user=root&password=igCotOwKrnqrA310&useSSL=false");  
             Statement stmt=con.createStatement();  
             ResultSet rs=stmt.executeQuery(
             		"SELECT RESOURCE , FLOOR(RAND()*(100)) FROM `RESOURCE_TIME_LOG` "
@@ -85,7 +85,7 @@ public class ShowResAnalysisDetails {
 		 String result="";
 	        try{  
 	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            Connection con=DriverManager.getConnection( "jdbc:mysql://connplantservice:3306/connplantsdb?user=root&password=VySU8WBweuVYNx3T&useSSL=false");  
+	            Connection con=DriverManager.getConnection("jdbc:mysql://globalmfgservice:3306/globalmfgdb?user=root&password=igCotOwKrnqrA310&useSSL=false");  
 	            Statement stmt=con.createStatement();  
 	            ResultSet rs=stmt.executeQuery(
 	            		"SELECT * FROM `RESOURCE_TIME_LOG` "
